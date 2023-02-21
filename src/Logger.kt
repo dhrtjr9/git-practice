b8536087710b33c1d491bbb4359edbe899965877
Lee Ok Seok <dhrtjr9@gmail.com>
2023-02-21 16:45:52 +0900

    회원가입 기능을 추가한다

---
+++ src/Logger.kt
@@ -0,0 +1,13 @@
+package src
+
+object Logger {
+
+    fun i(message: String) {
+        println("Log Info : $message")
+    }
+
+    fun e(errorMessage: String) {
+
+        println("Log Error : $errorMessage")
+    }
+}
